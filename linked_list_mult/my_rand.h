d9fd078f4b90c11970c4e50a60dc738020e9b9ab
Jean C <jean_16jean@hotmail.com>
2021-04-28 16:16:58 -0500

    Creoquetabien¿?

---
+++ linked_list_mult/my_rand.h
@@ -0,0 +1,31 @@
+#ifndef _MY_RAND_H_
+#define _MY_RAND_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MR_MULTIPLIER 279470273 
+#define MR_INCREMENT 0
+#define MR_MODULUS 4294967291U
+#define MR_DIVISOR ((double) 4294967291U)
+
+unsigned my_rand(unsigned* seed_p) {
+   long long z = *seed_p;
+   z *= MR_MULTIPLIER; 
+// z += MR_INCREMENT;
+   z %= MR_MODULUS;
+   *seed_p = z;
+   return *seed_p;
+}
+
+/* Function:      my_drand
+ * In/out arg:    seed_p
+ * Return value:  A new pseudo-random double in the range 0 - 1
+ */
+double my_drand(unsigned* seed_p) {
+   unsigned x = my_rand(seed_p);
+   double y = x/MR_DIVISOR;
+   return y;
+}
+
+#endif
